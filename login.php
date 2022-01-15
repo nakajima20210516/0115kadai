d246149c37a3ff2fed1e0a12ef123be84a29085b
nakajima20210516 <c200029@ccg.ac.jp>
2022-01-15 02:50:39 +0000

    login first commit

--- login.php
+++ login.php
@@ -21,11 +21,14 @@ if (!empty($_POST)) {
             $stmt->execute();
 			$member = $stmt->fetch(PDO::FETCH_ASSOC);
 //  ここにパスワードのチェック処理を完成させる
-//  		if( xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx ) {
+     		if(password_verify($password,$member['password'])) {
+     		     
+
+ 
 				// ログイン成功
 
 //  ここにセッションハイジャック対策を追加
-//
+            session_regenerate_id(true);
 
 				$_SESSION['id'] = $member['id'];
 				$_SESSION['time'] = time();
